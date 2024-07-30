cfca23b8f7cd1eee90655b8c8fc3b644db329772
Bogdan <bogdan.oprisiu2003@gmail.com>
2024-07-30 16:23:34 +0300

    Welcome page first draft

--- bts_app/webapp/controller/Login.controller.js
+++ bts_app/webapp/controller/Login.controller.js
@@ -1,30 +1,50 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
 
-  return Controller.extend("bts.btsapp.controller.Login", {
-    onInit: function () {},
+    return Controller.extend("bts.btsapp.controller.Login", {
+      onInit: function () {
+        // Retrieve the existing model or create a new one if it doesn't exist
+        var oModel = this.getView().getModel();
+        if (!oModel) {
+          oModel = new JSONModel();
+          this.getView().setModel(oModel);
+        }
 
-    onPressLogin: function () {
-      var username = this.getView().byId("usernameLogIn").getValue();
-      var password = this.getView().byId("passwordLogIn").getValue();
+        // Get the current data from the model
+        var oData = oModel.getData();
 
-      if (username && password) {
-        sap.m.MessageToast.show("Login successful!");
-        // Add your login logic here
-      } else {
-        sap.m.MessageToast.show("Please enter both username and password.");
-      }
-    },
+        // Add or update the HTML property
+        oData.HTML = oData.HTML || ""; // Ensure HTML exists in the model data
+        oData.customLogInHTML = "<h2>Welcome back</h2>";
 
-    onPressShowSignup: function () {
-      // Correctly retrieve the shared model
-      var oViewModel = this.getView().getModel("view");
-      if (oViewModel) {
-        oViewModel.setProperty("/showLoginForm", false);
-        oViewModel.setProperty("/showSignupForm", true);
-      } else {
-        console.error("View model not found");
-      }
-    },
-  });
-});
+        // Set the updated data back to the model
+        oModel.setData(oData);
+      },
+
+      onPressLogin: function () {
+        var username = this.getView().byId("usernameLogIn").getValue();
+        var password = this.getView().byId("passwordLogIn").getValue();
+
+        if (username && password) {
+          sap.m.MessageToast.show("Login successful!");
+          // Add your login logic here
+        } else {
+          sap.m.MessageToast.show("Please enter both username and password.");
+        }
+      },
+
+      onPressShowSignup: function () {
+        // Correctly retrieve the shared model
+        var oViewModel = this.getView().getModel("view");
+        if (oViewModel) {
+          oViewModel.setProperty("/showLoginForm", false);
+          oViewModel.setProperty("/showSignupForm", true);
+        } else {
+          console.error("View model not found");
+        }
+      },
+    });
+  }
+);
--- bts_app/webapp/controller/Signup.controller.js
+++ bts_app/webapp/controller/Signup.controller.js
@@ -1,30 +1,49 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
 
-  return Controller.extend("bts.btsapp.controller.Signup", {
-    onInit: function () {},
+    return Controller.extend("bts.btsapp.controller.Signup", {
+      onInit: function () {
+        // Retrieve the existing model or create a new one if it doesn't exist
+        var oModel = this.getView().getModel();
+        if (!oModel) {
+          oModel = new JSONModel();
+          this.getView().setModel(oModel);
+        }
 
-    onPressSignup: function () {
-      var username = this.getView().byId("usernameSignUp").getValue();
-      var password = this.getView().byId("passwordSignUp").getValue();
+        // Get the current data from the model
+        var oData = oModel.getData();
 
-      if (username && password) {
-        sap.m.MessageToast.show("Signup successful!");
-        // Add your signup logic here
-      } else {
-        sap.m.MessageToast.show("Please enter both username and password.");
-      }
-    },
+        // Add or update the customSignupHTML property
+        oData.customSignupHTML = "<h2>Hello</h2>";
 
-    onPressShowLogin: function () {
-      // Correctly retrieve the shared model
-      var oViewModel = this.getView().getModel("view");
-      if (oViewModel) {
-        oViewModel.setProperty("/showLoginForm", true);
-        oViewModel.setProperty("/showSignupForm", false);
-      } else {
-        console.error("View model not found");
-      }
-    },
-  });
-});
+        // Set the updated data back to the model
+        oModel.setData(oData);
+      },
+
+      onPressSignup: function () {
+        var username = this.getView().byId("usernameSignUp").getValue();
+        var password = this.getView().byId("passwordSignUp").getValue();
+
+        if (username && password) {
+          sap.m.MessageToast.show("Signup successful!");
+          // Add your signup logic here
+        } else {
+          sap.m.MessageToast.show("Please enter both username and password.");
+        }
+      },
+
+      onPressShowLogin: function () {
+        // Correctly retrieve the shared model
+        var oViewModel = this.getView().getModel("view");
+        if (oViewModel) {
+          oViewModel.setProperty("/showLoginForm", true);
+          oViewModel.setProperty("/showSignupForm", false);
+        } else {
+          console.error("View model not found");
+        }
+      },
+    });
+  }
+);
