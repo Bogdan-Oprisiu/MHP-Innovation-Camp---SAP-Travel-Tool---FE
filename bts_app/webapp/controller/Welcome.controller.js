172f3094b21a5a8869f901fd7b0edbe91522450f
Bogdan <bogdan.oprisiu2003@gmail.com>
2024-07-30 14:30:32 +0300

    Conditionally render LogIn and Sign Up

--- bts_app/webapp/controller/Welcome.controller.js
+++ bts_app/webapp/controller/Welcome.controller.js
@@ -1,7 +1,29 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
 
-  return Controller.extend("bts.btsapp.controller.Welcome", {
-    onInit: function () {},
-  });
-});
+    return Controller.extend("bts.btsapp.controller.Welcome", {
+      onInit: function () {
+        // Set up a JSONModel with a property to manage form visibility
+        var oViewModel = new JSONModel({
+          showLoginForm: true,
+          showSignupForm: false,
+        });
+        this.getView().setModel(oViewModel, "view");
+      },
+
+      onPressShowSignup: function () {
+        // Set properties to show signup form and hide login form
+        this.getView().getModel("view").setProperty("/showLoginForm", false);
+        this.getView().getModel("view").setProperty("/showSignupForm", true);
+      },
+
+      onPressShowLogin: function () {
+        // Set properties to show login form and hide signup form
+        this.getView().getModel("view").setProperty("/showLoginForm", true);
+        this.getView().getModel("view").setProperty("/showSignupForm", false);
+      },
+    });
+  }
+);
